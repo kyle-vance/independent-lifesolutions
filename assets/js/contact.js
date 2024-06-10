1e2948afb733905e72aa29dc3b98f911ed284673
kyle-vance <kylevance801@gmail.com>
2024-06-09 23:14:44 -0600

    media queries, google form

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -8,3 +8,47 @@ document.addEventListener("DOMContentLoaded", function() {
         })
         .catch(error => console.error("Error fetching contact section:", error));
 });
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     var contactForm = document.getElementById("contactForm");
+//     if (contactForm) {
+//         contactForm.addEventListener("submit", function(event) {
+//             event.preventDefault(); // Prevent form submission
+
+//             // Gather form data
+//             var formData = new FormData(this);
+
+//             // Send form data to server
+//             fetch("send_email.php", {
+//                 method: "POST",
+//                 body: formData
+//             })
+//             .then(response => {
+//                 if (response.ok) {
+//                     return response.text();
+//                 } else {
+//                     throw new Error("Network response was not ok.");
+//                 }
+//             })
+//             .then(data => {
+//                 // Show success notification
+//                 var notification = document.getElementById("notification");
+//                 if (notification) {
+//                     notification.classList.remove("d-none");
+//                     notification.innerText = data;
+//                 }
+//                 // Optionally, reset the form after successful submission
+//                 this.reset();
+//             })
+//             .catch(error => {
+//                 console.error("There was a problem with the fetch operation:", error);
+//                 // Show error message
+//                 alert("Error: Message not sent. Please try again later.");
+//             });
+//         });
+//     }
+// });
+
+
+
+
--- assets/js/idl.js
+++ assets/js/idl.js
@@ -1,34 +1,40 @@
-
 // #### Contact form javascript ####
 
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("contactForm").addEventListener("submit", function(event) {
-        event.preventDefault(); // Prevent form submission
+    var contactForm = document.getElementById("contactForm");
+    if (contactForm) {
+        contactForm.addEventListener("submit", function(event) {
+            event.preventDefault(); // Prevent form submission
 
-        
+            // Gather form data
+            var formData = new FormData(this);
 
-        // Gather form data
-        var formData = new FormData(this);
-
-        // Send form data to server
-        fetch("send_email.php", {
-            method: "POST",
-            body: formData
-        })
-        .then(response => {
-            if (response.ok) {
+            // Send form data to server
+            fetch("send_email.php", {
+                method: "POST",
+                body: formData
+            })
+            .then(response => {
+                if (response.ok) {
+                    return response.text();
+                } else {
+                    throw new Error("Network response was not ok.");
+                }
+            })
+            .then(data => {
                 // Show success notification
                 document.getElementById("notification").classList.remove("d-none");
+                document.getElementById("notification").innerText = data;
                 // Optionally, reset the form after successful submission
                 this.reset();
-            } else {
-                throw new Error("Network response was not ok.");
-            }
-        })
-        .catch(error => {
-            console.error("There was a problem with the fetch operation:", error);
-            // Show error message
-            alert("Error: Message not sent. Please try again later.");
+            })
+            .catch(error => {
+                console.error("There was a problem with the fetch operation:", error);
+                // Show error message
+                alert("Error: Message not sent. Please try again later.");
+            });
         });
-    });
+    }
 });
+
+
